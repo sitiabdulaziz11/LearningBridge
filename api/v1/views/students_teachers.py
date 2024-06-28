63a2a62d208195ed14aee8f2f5e3ca3b6a6b34c7
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-06-28 16:33:30 +0100

    Implemented the endpoints to link and unlink Teachers - Students

---
+++ api/v1/views/students_teachers.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+""" objects that handle RestFul API actions for Teacher - Students"""
+from models.student_models import Student
+from models.teacher_models import Teacher
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from api.v1.views.utils import token_required, require_user_class
+
+
+@app_views.route('/teachers/<teacher_id>/students', methods=['GET'],
+                 strict_slashes=False)
+@token_required
+def get_teacher_students(teacher_id, user):
+    """
+    Retrieves the list of all Student objects of a Teacher
+    """
+    teacher = storage.get(Teacher, teacher_id)
+
+    if not parent:
+        abort(404)
+
+    students = [student.to_dict() for student in teacher.students]
+
+    return make_response(jsonify(students), 200)
+
+
+@app_views.route('/teachers/<teacher_id>/students/<student_id>',
+                 methods=['POST'], strict_slashes=False)
+@token_required
+@require_user_class("Teacher")
+def link_student_teacher(teacher_id, student_id, user):
+    """
+    Links a Student object to a Teacher
+    """
+    teacher = storage.get(Teacher, teacher_id)
+
+    if not teacher:
+        abort(404)
+
+    student = storage.get(Student, student_id)
+
+    if not student:
+        abort(404)
+
+    if student in teacher.students:
+        return make_response(jsonify(student.to_dict()), 200)
+    else:
+        teacher.students.append(student)
+
+    storage.save()
+    return make_response(jsonify(student.to_dict()), 201)
+
+
+@app_views.route('/teachers/<teacher_id>/students/<students_id>',
+                 methods=['DELETE'], strict_slashes=False)
+@token_required
+@require_user_class("Administrator")
+def unlink_student_teacher(teacher_id, student_id, user):
+    """
+    Unlinks a Student object from a Teacher
+    """
+    teacher = storage.get(Teacher, teacher_id)
+
+    if not teacher:
+        abort(404)
+
+    student = storage.get(Student, student_id)
+
+    if not student:
+        abort(404)
+
+    if student not in teacher.students:
+        abort(404)
+    teacher.students.remove(student)
+
+    storage.save()
+    return make_response(jsonify({}), 200)
