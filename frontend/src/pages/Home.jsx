189da9946432d8469bccaef43e25bd384aa94dae
Olusegun Adeleke <oadeleke123@gmail.com>
2024-07-11 11:45:58 +0100

    prod v1.1.1 remove frontend. not working

--- frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from "../Layout/MainLayout.jsx";
-
-export default function Home() {
-    return (
-        <MainLayout>
-            <h2 className="text-3xl font-bold underline text-amber-800">Home page</h2>
-        </MainLayout>
-    )
-}
--- frontend/src/pages/Nopages.jsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from "../Layout/MainLayout";
-
-export default function Nopages() {
-    return (
-        <MainLayout>
-            <h2>Error 404: not found</h2>
-        </MainLayout>
-    )
-}
